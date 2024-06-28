39b8145ecd36dabda92e8ff91c2de7bdf3a02e23
Jessica Williams <jess@Jessicas-MBP.lan>
2024-06-28 12:59:04 -0400

    added navbar to layout

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Nav from '../components/navbar';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -8,7 +9,10 @@ import React from 'react';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Nav />
+        {children}
+        </body>
     </html>
   );
 }
